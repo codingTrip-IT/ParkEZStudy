196fff23be07ac6b543f690dac15d54bb1cae5b9
codingTrip <codingtrip@naver.com>
2025-05-08 14:36:18 +0900

    first commit

---
+++ src/main/java/com/example/parkezstudy/ParkEzStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkezstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkEzStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkEzStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parkezstudy/ParkEzStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkezstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkEzStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
